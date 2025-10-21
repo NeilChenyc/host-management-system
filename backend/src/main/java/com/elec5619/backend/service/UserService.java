14d15669197bdf23e242d970ee377ffe5555f026
jyou0588 <jyou0588@uni.sydney.edu.au>
2025-10-21 23:12:56 +1100

    Fix import order in UserService.java

--- backend/src/main/java/com/elec5619/backend/service/UserService.java
+++ backend/src/main/java/com/elec5619/backend/service/UserService.java
@@ -1,13 +1,11 @@
 package com.elec5619.backend.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 import com.elec5619.backend.dto.UserRegistrationDto;
 import com.elec5619.backend.dto.UserResponseDto;
